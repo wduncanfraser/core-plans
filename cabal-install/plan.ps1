fa61d7ffa7834cfc3cd18b6acc24c82740253d5e
W. Duncan Fraser <duncan@relianteng.io>
2021-03-09 16:15:30 -0600

    Rolled cabal-install forward

--- cabal-install/plan.ps1
+++ cabal-install/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="cabal-install"
 $pkg_origin="core"
-$pkg_version="3.0.0.0"
+$pkg_version="3.2.0.0"
 $pkg_license=@("BSD-3-Clause")
 $pkg_upstream_url="https://www.haskell.org/cabal/"
 $pkg_description="Command-line interface for Cabal and Hackage"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-$pkg_source="https://downloads.haskell.org/~cabal/cabal-install-latest/cabal-install-${pkg_version}-x86_64-unknown-mingw32.zip"
-$pkg_shasum="8889963ebef5e829d86329fdb59832c107efd117cf7862a605f2fe2d2360de1f"
+$pkg_source="https://downloads.haskell.org/~cabal/cabal-install-${pkg_version}/cabal-install-${pkg_version}-x86_64-unknown-mingw32.zip"
+$pkg_shasum="17778c3ade0482bc37f451eec326f8fce8fbe1f12b1d6aacb2e2b9e34786c054"
 
 $pkg_bin_dirs=@("bin")
 
